6ead876e4234c7735a9566ce308c7e23bf6d235b
Yi-An Chen <yachen@nuvoton.com>
2019-05-16 11:05:04 +0800

    Add ISP firmware projects
    Change-Id: Idf80f4ad8a62927721dba63dace227bee1473e72

---
+++ SampleCode/ISP/ISP_HID/targetdev.h
@@ -0,0 +1,14 @@
+/***************************************************************************//**
+ * @file     targetdev.h
+ * @brief    ISP support function header file
+ * @version  0x32
+ * @date     14, June, 2017
+ *
+ * @note
+ * Copyright (C) 2017-2018 Nuvoton Technology Corp. All rights reserved.
+ ******************************************************************************/
+#include "M4521.h"
+#include "isp_user.h"
+#include "hid_transfer.h"
+
+#define DetectPin   PD0
---
+++ SampleCode/ISP/ISP_UART/targetdev.h
@@ -0,0 +1,12 @@
+/***************************************************************************//**
+ * @file     targetdev.h
+ * @brief    ISP support function header file
+ *
+ * @note
+ * Copyright (C) 2017-2018 Nuvoton Technology Corp. All rights reserved.
+ ******************************************************************************/
+#include "M4521.h"
+#include "isp_user.h"
+
+
+#define DetectPin   PD0
