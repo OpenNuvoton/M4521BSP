938544aa70e6df05c3dc425396232bfa84c4f1da
MS10 CFLi0 <CFLi0@nuvoton.com>
2021-03-02 13:52:41 +0800

    USBD VCOM:Fixed UART TX FIFO issue
    Change-Id: Idc8bb8040c689424f7fcccc223c9df73dd0e30ef

--- SampleCode/StdDriver/USBD_VCOM_and_HID_Keyboard/main.c
+++ SampleCode/StdDriver/USBD_VCOM_and_HID_Keyboard/main.c
@@ -163,7 +163,7 @@ void UART0_IRQHandler(void)
     if(u32IntStatus & UART_INTSTS_THREIF_Msk)
     {
 
-        if(comTbytes)
+        if(comTbytes && (UART0->INTEN & UART_INTEN_THREIEN_Msk))
         {
             /* Fill the Tx FIFO */
             size = comTbytes;
@@ -262,9 +262,7 @@ void VCOM_TransferData(void)
             if(comThead >= TXBUFSIZE)
                 comThead = 0;
 
-            __set_PRIMASK(1);
             comTbytes--;
-            __set_PRIMASK(0);
 
             /* Enable Tx Empty Interrupt. (Trigger first one) */
             UART0->INTEN |= UART_INTEN_THREIEN_Msk;
